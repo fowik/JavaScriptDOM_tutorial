7495302079dca8a92377277e51caac7fe6b88b37
fowik <ilarimsa937@gmail.com>
2021-03-09 16:12:20 +0200

    Lesson 12

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ list.addEventListener('click', function(e){
         list.removeChild(li);
     }
 })
+
+// add books
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // append ti document'
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
