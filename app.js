86cd5ff696b35ee90724fdd38f4998d843f0569f
fowik <ilarimsa937@gmail.com>
2021-03-09 16:15:20 +0200

    Lesson 11

--- app.js
+++ app.js
@@ -10,21 +10,9 @@ list.addEventListener('click', function(e){
 
 // add books
 const addForm = document.forms['add-book'];
+
 addForm.addEventListener('submit',function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    
-    //create elements
-    const li = document.createElement('li');
-    const bookName = document.createElement('span');
-    const deleteBtn = document.createElement('span');
-
-    // add content
-    deleteBtn.textContent = 'delete';
-    bookName.textContent = value;
-
-    // append ti document'
-    li.appendChild(bookName);
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
+    console.log(value);
 });
